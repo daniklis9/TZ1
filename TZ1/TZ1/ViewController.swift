1a17211aae6877acd9e995c5672e009f96461551
Daniil <daniklis13@gmail.com>
2024-01-14 22:55:09 +0300

    created the project

---
+++ TZ1/TZ1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TZ1
+//
+//  Created by Даниил on 2024/01/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
